241841e83adbe98be0b2977b1d944e874be2a48c
mhor <maxime.horcholle@gmail.com>
2014-05-01 01:41:13 +0200

    use zippy lib

--- test/AlreadyExtract/Checker/AlreadyExtractCheckerTest.php
+++ test/AlreadyExtract/Checker/AlreadyExtractCheckerTest.php
@@ -2,7 +2,6 @@
 
 namespace AlreadyExtract\Checker;
 
-
 class AlreadyExtractCheckerTest extends \PHPUnit_Framework_TestCase
 {
 
@@ -14,8 +13,6 @@ class AlreadyExtractCheckerTest extends \PHPUnit_Framework_TestCase
 
     protected $errorArchive = 'c.zip';
 
-    protected $tooBigArchive = 'd.zip';
-
     public function __construct()
     {
         $this->archivesDir = __DIR__ . '/../../testArchive/';
@@ -25,37 +22,21 @@ class AlreadyExtractCheckerTest extends \PHPUnit_Framework_TestCase
     {
         $checker = new AlreadyExtractChecker();
         $checker->setArchiveFile($this->archivesDir . $this->successArchive);
-        $this->assertEquals(0, $checker->isAlreadyExtracted());
+        $this->assertEquals(0, $checker->isAlreadyExtracted($this->archivesDir));
     }
 
     public function testWarningIfExtractedDirectoryIsLowerThanArchive()
     {
         $checker = new AlreadyExtractChecker();
         $checker->setArchiveFile($this->archivesDir . $this->warningArchive);
-        $this->assertEquals(1, $checker->isAlreadyExtracted());
+        $this->assertEquals(1, $checker->isAlreadyExtracted($this->archivesDir));
     }
 
     public function testErrorIfArchiveINotExtract()
     {
         $checker = new AlreadyExtractChecker();
         $checker->setArchiveFile($this->archivesDir . $this->errorArchive);
-        $this->assertEquals(2, $checker->isAlreadyExtracted());
-    }
-
-    public function testSuccessIfToleranceIsLow()
-    {
-        $checker = new AlreadyExtractChecker();
-        $checker->setMinTolerance(0.99);
-        $checker->setArchiveFile($this->archivesDir . $this->warningArchive);
-        $this->assertEquals(0, $checker->isAlreadyExtracted());
-    }
-
-    public function testSuccessIfMaxToleranceIsHigh()
-    {
-        $checker = new AlreadyExtractChecker();
-        $checker->setMaxTolerance(2);
-        $checker->setArchiveFile($this->archivesDir . $this->tooBigArchive);
-        $this->assertEquals(0, $checker->isAlreadyExtracted());
+        $this->assertEquals(2, $checker->isAlreadyExtracted($this->archivesDir));
     }
 }
  
