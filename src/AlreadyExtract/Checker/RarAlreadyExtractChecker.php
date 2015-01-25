6400d594055111c2840a408569df2085ada0aa40
Maxime Horcholle <maxime.horcholle@gmail.com>
2015-01-25 23:06:22 +0100

    refactor

--- src/AlreadyExtract/Checker/RarAlreadyExtractChecker.php
+++ src/AlreadyExtract/Checker/RarAlreadyExtractChecker.php
@@ -51,42 +51,19 @@ class RarAlreadyExtractChecker implements AlreadyExtractCheckerInterface
      */
     public function isAlreadyExtracted($path)
     {
-        $guessedDirectory = str_replace($this->extension, '', $this->archiveFile);
+        $archive = \RarArchive::open($this->archiveFile);
+        $entries = $archive->getEntries();
+        foreach ($entries as $entry) {
+            if (!$this->fs->exists($path . $entry->getName())) {
+                return 2;
+            }
 
-        if (!$this->fs->exists($guessedDirectory)) {
-            return 2;
-        }
-
-        $archiveSize = filesize($this->archiveFile);
-
-        $maxSize = $archiveSize * $this->maxTolerance;
-        $minSize = $archiveSize - ($archiveSize * $this->minTolerance);
-
-        $extractDirSize = $this->getDirectorySize($guessedDirectory);
-        if ($extractDirSize < $minSize || $extractDirSize > $maxSize) {
-            return 1;
-        }
-
-        return 0;
-    }
-
-    /**
-     * @param string $path
-     * @return integer total size in bit of a directory
-     */
-    protected function getDirectorySize($path)
-    {
-        $totalBytes = 0;
-        $path = realpath($path);
-        if ($path!==false) {
-            foreach (
-                new \RecursiveIteratorIterator(
-                    new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS)
-                ) as $object
+            if (!$entry->isDirectory() &&
+                filesize($path . $entry->getName()) !== $entry->getUnpackedSize()
             ) {
-                $totalBytes += $object->getSize();
+                return 1;
             }
         }
-        return $totalBytes;
+        return 0;
     }
 }
